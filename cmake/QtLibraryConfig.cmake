41b69d7382b6c3e7dc16aab3f883e7f49f0b627e
Mads Ipsen <mads.ipsen@gmail.com>
2025-01-28 11:30:09 +0100

    - Removing explicit APPLE build conditionals

--- cmake/QtLibraryConfig.cmake
+++ cmake/QtLibraryConfig.cmake
@@ -1,14 +1,7 @@
-if(APPLE AND EXISTS /usr/local/opt/qt)
-  # Homebrew installs Qt5 (up to at least 5.9.1) in /usr/local/opt/qt. Ensure it can be found by CMake since
-  # it is not part of the default /usr/local prefix.
-  list(APPEND CMAKE_PREFIX_PATH "/usr/local/opt/qt")
-  
-elseif(LINUX)
-  list(APPEND CMAKE_PREFIX_PATH "$ENV{QT6_DIR}/lib/cmake")
-endif()
+# Use the env variable QT6_DIR to add Qt's cmake directory to CMAKE_PREFIX_PATH
+list(APPEND CMAKE_PREFIX_PATH "$ENV{QT6_DIR}/lib/cmake")
 
 message(STATUS "Qt6 cmake directory located in: ${CMAKE_PREFIX_PATH}" )
-
 
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
 set(CMAKE_AUTOMOC ON)
