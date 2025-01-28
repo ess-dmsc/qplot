93f66d61ecd8eb613fc38ea951145d31838b49a0
Mads Ipsen <mads.ipsen@gmail.com>
2025-01-28 11:19:56 +0100

    - Let CMake use the env variable QT6_DIR

--- cmake/QtLibraryConfig.cmake
+++ cmake/QtLibraryConfig.cmake
@@ -1,11 +1,16 @@
 if(APPLE AND EXISTS /usr/local/opt/qt)
-  # Homebrew installs Qt5 (up to at least 5.9.1) in
-  # /usr/local/opt/qt, ensure it can be found by CMake since
-  # it is not in the default /usr/local prefix.
+  # Homebrew installs Qt5 (up to at least 5.9.1) in /usr/local/opt/qt. Ensure it can be found by CMake since
+  # it is not part of the default /usr/local prefix.
   list(APPEND CMAKE_PREFIX_PATH "/usr/local/opt/qt")
+  
+elseif(LINUX)
+  list(APPEND CMAKE_PREFIX_PATH "$ENV{QT6_DIR}/lib/cmake")
 endif()
+
+message(STATUS "Qt6 cmake directory located in: ${CMAKE_PREFIX_PATH}" )
+
 
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
 set(CMAKE_AUTOMOC ON)
 
-find_package(Qt6 COMPONENTS Widgets PrintSupport Core5Compat REQUIRED)
+find_package(Qt6 REQUIRED COMPONENTS Widgets PrintSupport Core5Compat)
