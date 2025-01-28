dd52b3c7f7e9645e66b53c0960ab741d2adaba2c
Mads Ipsen <mads.ipsen@gmail.com>
2025-01-28 12:02:23 +0100

    - Add check to ensure that the user has set the path to the Qt6 location

--- cmake/QtLibraryConfig.cmake
+++ cmake/QtLibraryConfig.cmake
@@ -1,4 +1,8 @@
+
 # Use the env variable QT6_DIR to add Qt's cmake directory to CMAKE_PREFIX_PATH
+if(NOT DEFINED ENV{QT6_DIR})
+  message(FATAL_ERROR "The environment variable QT6_DIR has not been set. It should point to the root location of your Qt6 installation")
+endif()
 list(APPEND CMAKE_PREFIX_PATH "$ENV{QT6_DIR}/lib/cmake")
 
 message(STATUS "Qt6 cmake directory located in: ${CMAKE_PREFIX_PATH}" )
